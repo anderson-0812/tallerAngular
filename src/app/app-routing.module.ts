e47b68bfd98bb18dc7aff9b8c5d98f9245a51153
Anderson Jimenez <andersonmurdog@gmail.com>
2019-06-07 09:37:11 -0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { LoginComponent } from './components/login/login.component';
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  // Asignamos la ruta y componentes 
+  {path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
