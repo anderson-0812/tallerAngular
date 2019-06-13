a9c32a716e7e3078d3b410ec3245d62da5baf9d8
Anderson Jimenez <andersonmurdog@gmail.com>
2019-06-12 20:19:33 -0500

    empezamosa  crear el cierre de sesion e importar modulos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Global } from './services/global';
+import { UserService } from './services/user.service';
+// importe OnInit, DoCheck
+import { Component, OnInit, DoCheck } from '@angular/core';
+// importo estos modulos para cerrar sesion 
+import {Router, ActivatedRoute } from '@angular/router';
+import { Glob } from 'glob';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
+  public identidad: string;
+  public titulo: string;
+  public url: string;
+
+  // creo mi cosntructor 
+  constructor(private route: ActivatedRoute, private router: Router, private userService: UserService){
+    this.titulo = 'SACP-UTPL';
+    this.url = Global.url;
+  }
 }
