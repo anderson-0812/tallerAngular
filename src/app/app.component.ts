79bb22f84506cd7186a41fa9fc03e9e3025ed55f
Anderson Jimenez <andersonmurdog@gmail.com>
2019-06-12 20:44:48 -0500

    se creo lafuncion cerrar sesion ademas se valida si esta logeado o no el usuario apra kostrar el navbar, ademas con ngDoCheck refresca las variables de los datos del componete estatico esto es escecialapra chats

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Global } from './services/global';
 import { UserService } from './services/user.service';
 // importe OnInit, DoCheck
 import { Component, OnInit, DoCheck } from '@angular/core';
-// importo estos modulos para cerrar sesion 
+// importo estos modulos para cerrar sesion
 import {Router, ActivatedRoute } from '@angular/router';
 import { Glob } from 'glob';
 
@@ -17,9 +17,26 @@ export class AppComponent {
   public titulo: string;
   public url: string;
 
-  // creo mi cosntructor 
+  // creo mi cosntructor
   constructor(private route: ActivatedRoute, private router: Router, private userService: UserService){
     this.titulo = 'SACP-UTPL';
     this.url = Global.url;
+  }
+
+  // es para que se ejecute este componente se ejecuta es decir sirve para actibar componentes
+  ngOnInit() {
+    this.identidad = this.userService.obtenerIdentidad();
+    console.log(this.identidad);
+  }
+
+  // cuanse se haga cambios en un componente se actualiza los datos de losmcomponentes estaticos  q definamos este dentro 
+  ngDoCheck(){
+    this.identidad= this.userService.obtenerIdentidad();
+  }
+
+  cerrarSesion() {
+    localStorage.clear();
+    this.identidad = null;
+    this.router.navigate(['/login']);
   }
 }
